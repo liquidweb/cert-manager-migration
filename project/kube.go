fe8a0186706ee99928e0078681e57ae1812f14b0
Russ MacShane <rmacshane@liquidweb.com>
2018-11-27 15:07:49 -0600

    MWP-873: Added functionality to copy certs from one namespace to another

--- project/kube.go
+++ project/kube.go
@@ -3,9 +3,16 @@ package project
 import (
 	log "github.com/sirupsen/logrus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/flowcontrol"
 )
+
+var SchemeGroupVersion = schema.GroupVersion{Group: "stable.liquidweb.com", Version: "v1"}
 
 func kubeMigrationMain(conf Conf) {
 	if conf.Kube.ConfigFile == "" {
@@ -23,6 +30,13 @@ func kubeMigrationMain(conf Conf) {
 	}
 
 	migrateSecrets(client, conf)
+
+	certClient, err := newCertClient(kubeConfig)
+	if err != nil {
+		log.Fatalf("Failed to create certificate client: %v", err)
+	}
+
+	migrateCerts(certClient, conf)
 }
 
 func migrateSecrets(client *kubernetes.Clientset, conf Conf) {
@@ -46,4 +60,76 @@ func migrateSecrets(client *kubernetes.Clientset, conf Conf) {
 			log.Fatalf("Failed to create secret: %v", err)
 		}
 	}
+}
+
+func migrateCerts(certClient *rest.RESTClient, conf Conf) {
+	certs, err := getCertificates(certClient, conf.Kube.SourceNamespace)
+	if err != nil {
+		log.Fatalf("Error while retrieving certificate: %v.", err)
+	}
+
+	for _, cert := range certs {
+		log.Infof("Copying Certificate %s from %s to %s", cert.Metadata.Name, conf.Kube.SourceNamespace, conf.Kube.DestNamespace)
+
+		cert.Metadata.Namespace = conf.Kube.DestNamespace
+		cert.Metadata.ResourceVersion = ""
+
+		_, err := createCertificate(certClient, conf.Kube.DestNamespace, &cert)
+		if err != nil {
+			log.Fatalf("Failed to create certificate: %v", err)
+		}
+	}
+}
+
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		&Certificate{},
+		&CertificateList{},
+	)
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+	return nil
+}
+
+func newCertClient(cfg *rest.Config) (*rest.RESTClient, error) {
+	scheme := runtime.NewScheme()
+	SchemeBuilder := runtime.NewSchemeBuilder(addKnownTypes)
+	if err := SchemeBuilder.AddToScheme(scheme); err != nil {
+		return nil, err
+	}
+	config := *cfg
+	config.GroupVersion = &SchemeGroupVersion
+	config.APIPath = "/apis"
+	config.ContentType = runtime.ContentTypeJSON
+	config.NegotiatedSerializer = serializer.DirectCodecFactory{CodecFactory: serializer.NewCodecFactory(scheme)}
+	client, err := rest.RESTClientFor(&config)
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
+
+func getCertificates(certClient *rest.RESTClient, namespace string) ([]Certificate, error) {
+	rl := flowcontrol.NewTokenBucketRateLimiter(0.2, 3)
+	for {
+		rl.Accept()
+		req := certClient.Get().Resource("certificates").Namespace(namespace)
+
+		var certList CertificateList
+
+		err := req.Do().Into(&certList)
+
+		if err != nil {
+			log.Printf("Error while retrieving certificate: %v. Retrying", err)
+		} else {
+			return certList.Items, nil
+		}
+	}
+}
+
+func createCertificate(certClient *rest.RESTClient, namespace string, obj *Certificate) (*Certificate, error) {
+	result := &Certificate{}
+	err := certClient.Post().
+		Namespace(namespace).Resource("certificates").
+		Body(obj).Do().Into(result)
+	return result, err
 }
--- project/types.go
+++ project/types.go
@@ -5,8 +5,13 @@ import (
 	log "github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
+/*
+	PostgreSQL Database Schema
+ */
 type CertDetail struct {
 	gorm.Model
 	Domain string `gorm:"unique"`
@@ -25,6 +30,9 @@ type UserInfo struct {
 	Value string
 }
 
+/*
+	Configuration File Definition
+ */
 type Conf struct {
 	LogFile string `yaml:"log_file"`
 
@@ -49,6 +57,51 @@ type Conf struct {
 	}
 }
 
+/*
+	Certificate Custom Resource Definition
+ */
+type Certificate struct {
+	metav1.TypeMeta `json:",inline"`
+	Metadata        metav1.ObjectMeta `json:"metadata"`
+	Spec            CertificateSpec   `json:"spec"`
+	Status          CertificateStatus `json:"status,omitempty"`
+}
+
+type CertificateStatus struct {
+	Provisioned string `json:"provisioned,omitempty"`
+	CreatedDate string `json:"created,omitempty"`
+	ExpiresDate string `json:"expires,omitempty"`
+	ErrorMsg    string `json:"error_msg,omitempty"`
+	ErrorDate   string `json:"error_date,omitempty"`
+}
+
+type CertificateSpec struct {
+	Domain     string   `json:"domain"`
+	Provider   string   `json:"provider"`
+	Email      string   `json:"email"`
+	SecretName string   `json:"secretName"`
+	AltNames   []string `json:"altNames"`
+}
+
+type CertificateList struct {
+	metav1.TypeMeta `json:",inline"`
+	Metadata        metav1.ListMeta `json:"metadata"`
+	Items           []Certificate   `json:"items"`
+}
+
+func ( Certificate) DeepCopyObject() runtime.Object {
+	log.Warn("Certificate DeepCopyObject Not Implemented")
+	return nil
+}
+
+func ( CertificateList) DeepCopyObject() runtime.Object {
+	log.Warn("CertificateList DeepCopyObject Not Implemented")
+	return nil
+}
+
+/*
+	Utility Functions
+ */
 func (c *Conf) GetConf() *Conf {
 	yamlFile, err := ioutil.ReadFile("conf.yaml")
 	if err != nil {
